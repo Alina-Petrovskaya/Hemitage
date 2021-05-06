6a61e406e946b402aebdd89b40a61571a41fd788
Alina-Petrovskaya <alina.petrovskaia92@gmail.com>
2021-05-06 15:05:18 +0300

    Made UI For map cell for MainScreen

---
+++ Hemitage/Views/Main functionality/Main Screen/CollectionViewCells/Map/MapCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MapCollectionViewCell.swift
+//  Hemitage
+//
+//  Created by Alina Petrovskaya on 06.05.2021.
+//
+
+import UIKit
+
+class MapCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- Hemitage/Views/Main functionality/Main Screen/MainScreenViewController.swift
+++ Hemitage/Views/Main functionality/Main Screen/MainScreenViewController.swift
@@ -9,10 +9,16 @@ import UIKit
 
 class MainScreenViewController: UIViewController {
 
+    
+    @IBOutlet weak var songBottomView: TemplateSongView!
+    @IBOutlet weak var headerView: TemplateHeaderView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationController?.navigationBar.isHidden = true
+        
+        collectionView.register(UINib(nibName: "MapCollectionViewCell", bundle: .main), forCellWithReuseIdentifier: "MapCollectionViewCell")
     }
-
 }
