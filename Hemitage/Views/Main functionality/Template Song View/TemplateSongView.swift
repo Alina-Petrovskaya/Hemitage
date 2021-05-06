6dd20731c8584de2e6b50c2a507627affccb0faf
Alina-Petrovskaya <alina.petrovskaia92@gmail.com>
2021-05-06 12:22:20 +0300

    Added songView template Added TabBar navigation into RootViewController

--- Hemitage/Views/Main functionality/Template Song View/TemplateSongView.swift
+++ Hemitage/Views/Main functionality/Template Song View/TemplateSongView.swift
@@ -9,12 +9,47 @@ import UIKit
 
 class TemplateSongView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var nameSongLabel: UILabel!
+    @IBOutlet weak var albumSongLabel: UILabel!
+    @IBOutlet weak var imageSong: UIImageView!
+    
+    // MARK: - Life cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    
+    private func commonInit() {
+        Bundle.main.loadNibNamed(String(describing: TemplateSongView.self), owner: self)
+        updateUI()
+    }
+    
+    private func updateUI() {
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
+        imageSong.layer.cornerRadius = 8
+    }
+    
+    // MARK: - Actions
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        
+        let nameImageForButton = sender.imageView?.image == UIImage(systemName: "pause") ? "play" : "pause"
+        sender.setImage(UIImage(systemName: nameImageForButton), for: .normal)
+    }
+    
+    @IBAction func closeButtonTapped(_ sender: UIButton) {
+        self.isHidden = true
+    }
+    
+    
 
 }
--- Hemitage/Views/RootViewController.swift
+++ Hemitage/Views/RootViewController.swift
@@ -7,19 +7,35 @@
 
 import UIKit
 
-class RootViewController: UIViewController {
+class RootViewController: UITabBarController, UITabBarControllerDelegate {
     
-    enum NextViewController {
-        case loginVC, signInVC
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.barTintColor = .white
+        tabBar.tintColor = #colorLiteral(red: 0.902816236, green: 0.729167521, blue: 0.6777408719, alpha: 1)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
-        goToWelcomeController()
+        // goToWelcomeController()
+        presentContent()
+    }
+    
+    private func presentContent() {
+        guard let mainVC = MainScreenViewController.instantiate() else { return }
+        
+        mainVC.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "home-fill"), selectedImage: nil)
+        
+        let navigationController = UINavigationController(rootViewController: mainVC)
+        
+        
+        let controllers = [mainVC, sVC]
+        self.viewControllers = controllers
     }
     
     private func goToWelcomeController() { // Here we'll deside what controller we'll show
-        guard let vc = MainScreenViewController.instantiate()
+        guard let vc = WelcomeViewController.instantiate()
         else {
             print("Can't create path to Welcome VC")
             return
@@ -28,5 +44,9 @@ class RootViewController: UIViewController {
         let navVC = UINavigationController(rootViewController: vc)
         navVC.modalPresentationStyle = .fullScreen
         present(navVC, animated: true, completion: nil)
+    }
+    
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        return true;
     }
 }
