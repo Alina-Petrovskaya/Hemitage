fe8c2bd5fc899641df20874aa4f85ed101c0f8e3
Alina-Petrovskaya <alina.petrovskaia92@gmail.com>
2021-06-18 16:56:53 +0300

    Commit changes

---
+++ Hemitage/Modules/Main/PaymentScreen/Cells/PaymentCell.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentCell.swift
+//  Hemitage
+//
+//  Created by Alina Petrovskaya on 18.06.2021.
+//
+
+import UIKit
+
+class PaymentCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
